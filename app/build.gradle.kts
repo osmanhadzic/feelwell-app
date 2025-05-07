953ab66ddf00948d07064b3cd1eea8405a962d79
Osman Hadzic <osman.hadzic@secomind.com>
2025-05-07 22:44:41 +0200

    Init Jetpack Navigation Component
    Signed-off-by: Osman Hadzic <osman.hadzic@secomind.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,11 @@ android {
 
     buildFeatures {
         viewBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 }
 
@@ -102,10 +107,25 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.annotation:annotation:1.7.1")
 
+    // Jetpack Compose
+    implementation("androidx.compose.ui:ui:1.5.3")
+    implementation("androidx.compose.material:material:1.5.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
+
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.8")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.8.8")
+
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.3")
+
     // Testing
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.3")
+
 }
 
 spotless {
